07458c56e9b21b98cfc038ba789f9317c91e2cab
Evgeny Zelenin <transingularity@gmail.com>
2016-09-24 20:19:58 +0400

    Added code for multiplication two matrix

--- SomeSol/Matrix/Program.cs
+++ SomeSol/Matrix/Program.cs
@@ -10,6 +10,38 @@ namespace Matrix
     {
         static void Main(string[] args)
         {
+            int[,] matrix1 = new int[5, 5];
+            int[,] matrix2 = new int[5, 5];
+            int[,] matrixC = new int[5, 5];
+
+            Random rand = new Random();
+
+            for (int i = 0; i < 5; i++)
+            {
+                for (int j = 0; j < 5; j++)
+                {
+                    matrix1[i, j] = rand.Next(1, 15);
+                }
+            }
+
+            for (int i = 0; i < 5; i++)
+            {
+                for (int j = 0; j < 5; j++)
+                {
+                    matrix2[i, j] = rand.Next(1, 15);
+                }
+            }
+
+            for (int i = 0; i < 5; i++)
+            {
+                for (int j = 0; j < 5; j++)
+                {
+                    matrixC[i, j] = matrix1[i, j] * matrix2[i, j];
+                    Console.Write("{0}\t", matrixC[i, j]);
+                }
+                Console.WriteLine();
+            }
+            Console.ReadLine();
         }
     }
 }
